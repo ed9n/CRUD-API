9b6f780bdf56806db1445ac8137664a03557aaf6
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-15 20:37:44 +0300

    feat: add create

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import { createRequire } from "module";
+import { v4 } from 'uuid';
+import { writeToData } from '../../utils/index.js';
 
 const require = createRequire(import.meta.url);
 const users = require('../../data/users.json');
@@ -12,7 +14,17 @@ export const findAllUsers = () => {
 export const findUserById = (id) => {
     return new Promise((res, rej) => {
         const user = users.find((el) => el.id === id);
-        res(user)
+        res(user);
     });
-}
+};
+
+export const create = (user) => {
+    return new Promise((res, rej) => {
+        const newUser = { id: v4(), ...user };
+        users.push(newUser);
+        writeToData('./data/users.json', users);
+        res(newUser);
+    });
+};
+
 
