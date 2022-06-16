b93c7a32b23d3e170a86d16f3f5d7969c19d9cbc
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-16 18:38:58 +0300

    feat: add change

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -6,24 +6,39 @@ import { User } from '../interfaces/interfaces';
 
 
 export const findAllUsers = () => {
-    return new Promise((res, rej) => {
+    return new Promise<User[]>((res, rej) => {
         res(users);
     });
 };
 
 export const findUserById = (id: string) => {
-    return new Promise((res, rej) => {
+    return new Promise<User>((res, rej) => {
         const user = users.find((el) => el.id === id);
         res(user);
     });
 };
 
 export const create = (user: User) => {
-    return new Promise((res, rej) => {
+    return new Promise<User>((res, rej) => {
         const newUser = { id: v4(), ...user };
         users.push(newUser);
         writeToData('./data/users.json', users);
         res(newUser);
+    });
+};
+
+export const change = (oldUser: User, newUser: User) => {
+    return new Promise<User>((res, rej) => {
+        const changedUser = newUser;
+        const user = oldUser;
+
+        user.name = changedUser.name;
+        user.age = changedUser.age;
+        user.hobbies = changedUser.hobbies;
+
+        writeToData('./data/users.json', users)
+
+        res(user);
     });
 };
 
