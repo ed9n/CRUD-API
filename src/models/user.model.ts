daf0274eeabddd5fad65df5c4f7292cfe4238087
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-17 01:30:17 +0300

    feat: add remove

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -3,22 +3,20 @@ import { writeToData } from '../utils/index';
 import users from '../../data/users.json';
 import { User } from '../interfaces/interfaces';
 
-
-
 export const findAllUsers = () => {
-    return new Promise<User[]>((res, rej) => {
+    return new Promise<Array<User>>((res, rej) => {
         res(users);
     });
 };
 
 export const findUserById = (id: string) => {
     return new Promise<User>((res, rej) => {
-        const user = users.find((el) => el.id === id);
+        const user: User = users.find((el) => el.id === id);
         res(user);
     });
 };
 
-export const create = (user: User) => {
+export const create = (user: User): Promise<User> => {
     return new Promise<User>((res, rej) => {
         const newUser = { id: v4(), ...user };
         users.push(newUser);
@@ -29,8 +27,8 @@ export const create = (user: User) => {
 
 export const change = (oldUser: User, newUser: User) => {
     return new Promise<User>((res, rej) => {
-        const changedUser = newUser;
-        const user = oldUser;
+        const changedUser: User = newUser;
+        const user: User = oldUser;
 
         user.name = changedUser.name;
         user.age = changedUser.age;
@@ -39,6 +37,15 @@ export const change = (oldUser: User, newUser: User) => {
         writeToData('./data/users.json', users)
 
         res(user);
+    });
+};
+
+export const remove = (user: User) => {
+    return new Promise<Array<User>>((res, rej) => {
+        const newArray = users.filter((item) => item.id !== user.id)
+        console.log(newArray)
+        writeToData('./data/users.json', newArray);
+        res(newArray)
     });
 };
 
