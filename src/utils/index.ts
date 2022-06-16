75313ed6fb0ca99f02474101cfcdc618e2a63138
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-16 18:40:03 +0300

    refactor: add type for pth and content

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,8 @@
 import { createWriteStream } from 'fs';
+import { User } from '../interfaces/interfaces';
 
 
-export const writeToData = async (puth: any, content: any) => {
+export const writeToData = async (puth: string, content: User[]) => {
     try {
         const writableStream = createWriteStream(puth, { flags: 'w' });
         writableStream.write(JSON.stringify(content));
