45a95d1eb04cdd4e96937a78236d9b24eae41b43
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-19 04:30:44 +0300

    refator: formatting code

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -4,21 +4,27 @@ import { users } from '../../data/users';
 
 export const findAllUsers = () => {
     return new Promise<Array<User>>((res, rej) => {
+
         res(users);
+
     });
 };
 
 export const findUserById = (id: string) => {
     return new Promise<User>((res, rej) => {
+
         const user: User = users.find((el) => el.id === id);
 
         res(user);
+
     });
 };
 
 export const create = (user: User) => {
     return new Promise<User>((res, rej) => {
+
         const newUser = { id: v4(), ...user };
+
         users.push(newUser);
 
         res(newUser);
@@ -27,6 +33,7 @@ export const create = (user: User) => {
 
 export const change = (oldUser: User, newUser: User) => {
     return new Promise<User>((res, rej) => {
+
         const changedUser: User = newUser;
         const user: User = oldUser;
 
@@ -35,16 +42,21 @@ export const change = (oldUser: User, newUser: User) => {
         user.hobbies = changedUser.hobbies;
 
         res(user);
+
     });
 };
 
 export const remove = (user: User) => {
     return new Promise<Array<User>>((res, rej) => {
+
         const index = users.indexOf(user)
+
         if (index !== -1) {
             users.splice(index, 1);
         }
+
         res(users)
+
     });
 };
 
