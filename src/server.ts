b0a91dc99611c380f70bf80c6d68604eea42679e
Eduard Rzhautski <eduard.rzhautski@gmail.com>
2022-06-19 04:35:07 +0300

    feat: add new File with name server.ts and moved from app.ts to server.ts (server.listen)

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,7 @@
-import { createServer, IncomingMessage, ServerResponse } from 'http';
 import dotenv from 'dotenv';
-import { changeUser, createUser, getUser, getUsers, removeUser } from './controllers/user.controller';
-
-
-const server = createServer((req: IncomingMessage, res: ServerResponse) => {
-    const id = req.url.split('/')[3];
-
-    if (req.url === `/api/users` && req.method === 'GET') {
-        getUsers(req, res);
-    } else if (req.url === `/api/users/${id}` && req.method === 'GET') {
-        getUser(req, res, id);
-    } else if (req.url === `/api/users` && req.method === 'POST') {
-        createUser(req, res)
-    } else if (req.url === `/api/users/${id}` && req.method === 'PUT') {
-        changeUser(req, res, id)
-    } else if (req.url === `/api/users/${id}` && req.method === 'DELETE') {
-        removeUser(req, res, id)
-    } else {
-        res.writeHead(404, { 'Content-Type': 'application/json' });
-        res.end(JSON.stringify({ message: 'Root not found' }));
-    }
-});
+import { server } from './app';
 
 dotenv.config();
 const PORT = process.env.PORT;
 
 server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
